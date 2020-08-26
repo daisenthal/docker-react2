5ebc131c5951ca3ac1ac36ac82d73bc377d321ca
daisenthal <david.aisenthal@intrasoft.intl.com>
2020-08-26 22:10:03 +0300

    feature branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          My name is Simon
+          My name is David
         </p>
       </div>
     );
